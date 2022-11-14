d696faf191d0f0c5d0e15bba0be3bb4e7c657a21
ghostblizt <carlos.marcano@live.com>
2022-11-14 14:58:32 -0300

    Solucion de errores en consola

--- src/components/NavBar/Logo.jsx
+++ src/components/NavBar/Logo.jsx
@@ -4,13 +4,11 @@ import logo from './../../assets/img/logo.png'
 export const Logo = ({ clases }) =>{
     
     return(
-        <Link to='/'>
             <img 
                 src={ logo }
                 alt={ logo }
                 className={ clases } 
                 width='250px'
             />
-        </Link>
     )
 } 
